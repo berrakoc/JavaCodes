dde8d6de8f9318ef9fab6593968ad93f97122606
Berra Koç <144827481+berrakoc@users.noreply.github.com>
2024-06-14 22:26:53 +0300

    Add files via upload

---
+++ Books/Book.java
@@ -0,0 +1,33 @@
+
+public class Book {
+
+    private String name;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean equals (Object obj){
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book book = (Book) obj;
+        return publicationYear == book.publicationYear && name.equals(book.name);
+    }
+    public String toString() {
+        return name + " (" + publicationYear + ")";
+    }
+}
